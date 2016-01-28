a7ca9e432530547404576744fa1540fc168e6d7a
ruv-prog-so <r.piatnitsky@gmail.com>
2016-01-28 14:57:14 -0800

    Initial Commit of Project Solution

---
+++ CVisProject2/CVisProject2/Source.cpp
@@ -0,0 +1,42 @@
+/* ===== Written By Ruben Piatnitsky =====
+
+---Face Detection and Recognition---
+This project needs to develop a system that can automatically detect a face from an input 
+image and recognize the face. A wide variety of algorithms and codes are available for 
+face detection and recognition. You can use all the online code.
+
+Grading policy:
+a. Implement face detection. (10 points)
+b. Test at least three different face recognition algorithms. (15 points for each algorithm)
+c. In-class presentation (0-10 points)
+d. Project report (0-20 points)
+
+References:
+[1] OpenCV
+[2] http://www.face-rec.org/
+*/
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2\features2d\features2d.hpp"
+#include "opencv2\objdetect\objdetect.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
+
+
+// This is Project 2 - Facial Recognition
+///
+///
+int main()
+{
+
+
+	// Wait for a key to be pressed to exit application
+	waitKey(0);
+	return 0;
+}
